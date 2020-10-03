36cf39995d2e712b36c80622bf682a9ebb0a8cdc
Ariana <ariana@astele.co.uk>
2020-10-03 09:54:16 +0100

    Improve progress.py and refactor steps files

--- opendoors/tests/test_progress.py
+++ opendoors/tests/test_progress.py
@@ -11,6 +11,10 @@ from opendoors.config import ArchiveConfig
 from opendoors.progress import continue_from_last, update_done_steps, get_next_step
 from opendoors.step_base import StepBase, StepInfo
 
+test_logger = MagicMock()
+test_config: ConfigParser = ArchiveConfig(test_logger, "efiction", "opendoors/tests/test_data").config
+test_sql = MagicMock()
+
 
 class Step1(StepBase):
     """Test Step 1"""
@@ -33,9 +37,6 @@ steps = {
     '02': {'info': StepInfo(next_step='None', step_description='Step 2', step_number='02'), 'class': Step2}
 }
 
-test_logger = MagicMock()
-test_sql = MagicMock()
-
 
 class TestProgress(TestCase):
     def tearDown(self) -> None:
@@ -54,38 +55,27 @@ class TestProgress(TestCase):
     # This patch responds '2' to every prompt and makes it run all the steps in turn
     @patch('builtins.input', lambda *args: '2')
     def test_continue_from_last(self):
-        test_config = ArchiveConfig(test_logger, "test1", "opendoors/tests/test_output").config
         continue_from_last(test_config, test_logger, test_sql, steps)
-        self.assertSetEqual(set("01, 02".split(', ')), set(test_config['Processing']['done_steps'].split(', ')),
-                            "continue_from_last should update the done_steps config")
+        self.assertSetEqual(set("01, 02".split(', ')), set(test_config['Processing']['done_steps'].split(', ')))
 
-    @patch('builtins.input', lambda *args: 'Full Archive Name')
     def test_update_done_steps(self):
-        test_config = ArchiveConfig(test_logger, "test2", "opendoors/tests/test_output").config
-        test_config["Processing"]["done_steps"] = "01, 02"
         done_steps = update_done_steps(test_config, ["01", "02"], "03")
-        self.assertEqual("01, 02, 03", done_steps, "update_done_steps should return the done steps as a string")
+        self.assertEqual("01, 02, 03", done_steps)
 
     @patch('builtins.input', lambda *args: '2')
     def test_get_next_step_after_01(self):
-        test_config = ArchiveConfig(test_logger, "test3", "opendoors/tests/test_output").config
         next_step, done_steps = get_next_step(test_config, "01")
-        self.assertEqual("01", next_step, "next_step should be set to 01")
-        self.assertEqual([], done_steps, "done_steps should be reset to empty")
+        self.assertEqual("01", next_step)
+        self.assertEqual([], done_steps)
 
     @patch('builtins.input', lambda *args: '2')
     def test_get_next_step_after_02(self):
-        test_config = ArchiveConfig(test_logger, "test4", "opendoors/tests/test_output").config
-        test_config["Processing"]["done_steps"] = "01, 02"
         next_step, done_steps = get_next_step(test_config, "02")
-        self.assertEqual("02", next_step, "next_step should be set to 02")
-        self.assertListEqual(test_config["Processing"]["done_steps"].split(", "), done_steps,
-                             "done_steps should be the value currently in the config")
+        self.assertEqual("02", next_step)
+        self.assertListEqual(['01', '02'], done_steps)
 
     @patch('builtins.input', lambda *args: '1')
     def test_get_next_step_with_restart(self):
-        test_config = ArchiveConfig(test_logger, "test5", "opendoors/tests/test_output").config
-        test_config["Processing"]["done_steps"] = "01, 02"
         next_step, done_steps = get_next_step(test_config, "02")
-        self.assertEqual("01", next_step, "next_step should be set to 01")
-        self.assertEqual([], done_steps, "done_steps should be reset to empty")
+        self.assertEqual("01", next_step)
+        self.assertEqual([], done_steps)
