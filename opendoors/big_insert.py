876b5eca9d94ce6b76c742c6871164ae16f083ba
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-08-31 21:02:43 +0200

    Add encoding to big_insert

--- opendoors/big_insert.py
+++ opendoors/big_insert.py
@@ -16,7 +16,7 @@ class BigInsert:
         self._sql.ensure_local_infile()
         self._database = database
         self._columns = columns
-        self._tempfile = NamedTemporaryFile("w+", delete=False, suffix=".otw.tmp")
+        self._tempfile = NamedTemporaryFile("w+", delete=False, suffix=".otw.tmp", encoding="utf-8")
         # convert windows slashes to unix becouse windows needs it like that?
         windows_friendly_name = self._tempfile.name.replace("\\", "/")
         # We are storing data as a `tab separated` file
