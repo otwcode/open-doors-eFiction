5426e38e5e0313698a50fe74415d5bcecda22066
brianna.dardin <brianna.dardin@gmail.com>
2023-01-22 14:31:18 -0800

    Updated test_chapters to handle non-utf-8 file

--- efiction/tests/test_chapters.py
+++ efiction/tests/test_chapters.py
@@ -14,9 +14,10 @@ from opendoors.utils import get_full_path, normalize, remove_output_files
 def get_data():
     with open(get_full_path("efiction/tests/test_data/efiction_chapters.json"), "r", encoding="utf-8") as file:
         old_data_with_text = json.loads(file.read())
-
     old_data_no_text = copy.deepcopy(old_data_with_text)
-    new_data = []
+
+    new_data_full = []
+    new_data_missing = []
     for i, chap in enumerate(old_data_with_text):
         old_data_no_text[i]["storytext"] = ""
 
@@ -34,12 +35,16 @@ def get_data():
             "notes": chap["notes"],
             "url": ""
         }
-        new_data.append(chap_dict)
+        new_data_full.append(chap_dict)
 
-    return old_data_with_text, old_data_no_text, new_data
+        # this is the chapter that fails due to wrong encoding
+        if chap["chapid"] != 842:
+            new_data_missing.append(chap_dict)
+
+    return old_data_with_text, old_data_no_text, new_data_full, new_data_missing
 
 
-old_data_with_text, old_data_no_text, new_data = get_data()
+old_data_with_text, old_data_no_text, new_data_full, new_data_missing = get_data()
 test_logger = MagicMock()
 test_config = ArchiveConfig(test_logger, "efiction", "efiction/tests/test_data").config
 
@@ -48,7 +53,6 @@ test_config = ArchiveConfig(test_logger, "efiction", "efiction/tests/test_data")
 def mock_sql():
     with patch('opendoors.mysql.SqlDb', autospec=True) as mock_Sql:
         sql = mock_Sql(test_config, test_logger)
-        sql.execute_and_fetchall.return_value = new_data
         yield sql
 
 
@@ -63,25 +67,27 @@ def mock_insert(mock_sql):
 def test_chapters_with_text_main_method(mock_method, mock_sql, mock_insert):
     mock_sql.read_table_to_dict.return_value = old_data_with_text
     mock_sql.read_table_with_total.return_value = (old_data_with_text, 0, len(old_data_with_text))
+    mock_sql.execute_and_fetchall.return_value = new_data_full
 
     efiction_chapters = EFictionChapters(test_config, test_logger, mock_sql)
-    assert efiction_chapters.load_chapters("test_path")
+    assert efiction_chapters.load_chapters("test_path"), "load_chapters doesn't return true"
     mock_method.assert_called_once()
 
 
 def test_chapters_with_text_processing(mock_sql, mock_insert):
     mock_sql.read_table_to_dict.return_value = old_data_with_text
     mock_sql.read_table_with_total.return_value = (old_data_with_text, 0, len(old_data_with_text))
+    mock_sql.execute_and_fetchall.return_value = new_data_full
 
     efiction_chapters = EFictionChapters(test_config, test_logger, mock_sql)
     working_chapters = efiction_chapters.load_og_chapters_into_db()
-    assert len(new_data) == len(working_chapters)
+    assert len(new_data_full) == len(working_chapters), "Returned chapters from method don't match the number of expected chapters"
 
     mock_sql.read_table_to_dict.assert_called_with("efictiontest_test_step_original_efiction_edited", "chapters")
     mock_sql.read_table_with_total.assert_called_with("efictiontest_test_step_original_efiction_edited", "chapters")
     mock_sql.execute_and_fetchall.assert_called_with("efictiontest_working_open_doors", "SELECT * FROM chapters;")
 
-    assert mock_insert.addRow.call_count == len(new_data)
+    assert mock_insert.addRow.call_count == len(new_data_full), "addRow calls don't match the number of expected chapters"
     mock_insert.send.assert_called_once()
 
 
@@ -89,23 +95,27 @@ def test_chapters_with_text_processing(mock_sql, mock_insert):
 def test_chapters_no_text_main_method(mock_method, mock_sql, mock_insert):
     mock_sql.read_table_to_dict.return_value = old_data_no_text
     mock_sql.read_table_with_total.return_value = (old_data_no_text, 0, len(old_data_no_text))
+    mock_sql.execute_and_fetchall.return_value = new_data_missing
 
     efiction_chapters = EFictionChapters(test_config, test_logger, mock_sql)
-    assert efiction_chapters.load_chapters("test_path")
+    assert efiction_chapters.load_chapters("test_path"), "load_chapters doesn't return true"
     mock_method.assert_called_once()
 
 
-def test_chapters_no_text_processing(mock_sql, mock_insert):
+def test_chapters_no_text_processing(monkeypatch, mock_sql, mock_insert):
     mock_sql.read_table_to_dict.return_value = old_data_no_text
     mock_sql.read_table_with_total.return_value = (old_data_no_text, 0, len(old_data_no_text))
+    mock_sql.execute_and_fetchall.return_value = new_data_missing
+
+    monkeypatch.setattr('builtins.input', lambda _: "YES, DO AS I SAY!")
 
     efiction_chapters = EFictionChapters(test_config, test_logger, mock_sql)
     working_chapters = efiction_chapters.load_chapter_text_into_db(efiction_chapters.list_chapter_files())
-    assert len(new_data) == len(working_chapters)
+    assert len(new_data_missing) == len(working_chapters), "Returned chapters from method don't match the number of expected chapters"
 
     mock_sql.read_table_to_dict.assert_called_with("efictiontest_test_step_original_efiction_edited", "chapters")
     mock_sql.read_table_with_total.assert_called_with("efictiontest_test_step_original_efiction_edited", "chapters")
     mock_sql.execute_and_fetchall.assert_called_with("efictiontest_working_open_doors", "SELECT * FROM chapters;")
 
-    assert mock_insert.addRow.call_count == len(new_data)
+    assert mock_insert.addRow.call_count == len(new_data_missing), "addRow calls don't match the number of expected chapters"
     mock_insert.send.assert_called_once()
